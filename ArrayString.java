d854a756a81a642bf21280d692d1d57d3770f269
Swatiyash <60690162+Swatiyash@users.noreply.github.com>
2020-02-12 16:15:52 +0530

    Add files via upload

---
+++ ArrayString.java
@@ -0,0 +1,23 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class ArrayString 
+{
+
+	public static void main(String[] args) 
+	{
+		
+
+		String colors[]= {"Red","Blue","White"};
+		
+		for(String Clr:colors)
+		{
+			
+			System.out.println(Clr);
+			
+		}
+
+	}
+
+}
