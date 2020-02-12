fe31438bea3b12d41b56318a2ebfdca28978ff72
Swatiyash <60690162+Swatiyash@users.noreply.github.com>
2020-02-12 16:20:03 +0530

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,26 @@
+package Basic;
+
+class Constructor
+{
+    int  value1;
+    int  value2;
+    
+    Constructor()//Constructor
+    {
+       value1 = 10;
+       value2 = 20;
+       System.out.println("Inside Constructor");
+   }
+
+   public void display()//Method
+   {
+      System.out.println("Value1 === "+value1);
+      System.out.println("Value2 === "+value2);
+   }
+
+   		public static void main(String args[])
+ 		{
+	 			Constructor d1 = new Constructor();
+	 			d1.display();
+ 		}
+}
